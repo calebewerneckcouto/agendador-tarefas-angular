cea4312a2f14f2964e3d00ba60a47be12b1f4d89
Calebe <calebewerneck@hotmail.com>
2026-09-10 12:56:24 -0300

    create form logic

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,20 +1,35 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule, MatAnchor } from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { PasswordField } from '../../shared/components/password-field/password-field';
+import { ReactiveFormsModule,FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
-  imports: [MatCardModule, MatAnchor, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule,PasswordField],
+  imports: [MatCardModule, MatAnchor, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, PasswordField,ReactiveFormsModule],
   selector: 'app-register',
   styleUrl: './register.scss',
   templateUrl: './register.html',
-  encapsulation:ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent {
-  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.
-A small, agile dog that copes very well with mountainous terrain, and was originally bred for hunting.`;
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      password: ['']
+    });
+  }
+
+  get passwordControl():FormControl{
+    return this.form.get('password') as FormControl
+  }
+
+  submit(){
+    console.log(this.form.value)
+  }
 }
--- src/app/shared/components/password-field/password-field.ts
+++ src/app/shared/components/password-field/password-field.ts
@@ -1,17 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { FormControl,ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 
 @Component({
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule,ReactiveFormsModule],
   selector: 'app-password-field',
   styleUrl: './password-field.scss',
   templateUrl: './password-field.html',
 })
 export class PasswordField {
   hide = signal(true);
+
+  @Input({required:true}) control!: FormControl;
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
